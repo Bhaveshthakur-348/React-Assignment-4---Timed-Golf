492de100c5a51afc1f6f3f7f086042a64dd079d1
Bhaveshthakur-348 <bhaveshthakur348@gmail.com>
2021-02-26 22:32:33 +0530

    golf

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,81 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = {
+      time: 0,
+      x: 0,
+      y: 0,
+      start: false,
+    };
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.shiftBall = this.shiftBall.bind(this);
   }
+  shiftBall(event) {
+    event.preventDefault();
+    if (!this.state.start) {
+      return;
+    }
+    if (this.state.x === 250 && this.state.y === 250) {
+      return;
+    }
+    if (event.keyCode === 37) {
+      if (this.state.x === 250 && this.state.y - 5 === 250) {
+        clearInterval(this.interval);
+      }
+      this.setState({ y: this.state.y - 5 });
+    }
+    if (event.keyCode === 39) {
+      if (this.state.x === 250 && this.state.y + 5 === 250) {
+        clearInterval(this.interval);
+      }
+      this.setState({ y: this.state.y + 5 });
+    }
+    if (event.keyCode === 38) {
+      if (this.state.x - 5 === 250 && this.state.y === 250) {
+        clearInterval(this.interval);
+      }
+      this.setState({ x: this.state.x - 5 });
+    }
+    if (event.keyCode === 40) {
+      if (this.state.x + 5 === 250 && this.state.y === 250) {
+        clearInterval(this.interval);
+      }
+      this.setState({ x: this.state.x + 5 });
+    }
+  }
+  buttonClickHandler() {
+    this.interval = setInterval(() => {
+      this.setState({ time: this.state.time + 1 });
+    }, 1000);
+    console.log(this.state.time);
+    this.setState({ start: true });
+  }
+
   componentDidMount() {
-    
+    document.addEventListener("keydown", (event) => this.shiftBall(event));
   }
 
-  componentWillUnmount() {
-    
-  }
-
-
+  componentWillUnmount() {}
 
   render() {
     return (
- <>
-</>
+      <>
+        <p className="heading-timer">{this.state.time}</p>
+
+        <div
+          className="ball"
+          style={{ top: this.state.x + "px", left: this.state.y + "px" }}
+        ></div>
+        <div className="hole"></div>
+        {!this.state.start && (
+          <button
+            className="start ballProvider"
+            onClick={this.buttonClickHandler}
+          >
+            Start
+          </button>
+        )}
+      </>
     );
   }
 }
